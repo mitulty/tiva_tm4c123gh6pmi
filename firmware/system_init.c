83c886a2cd89d73949af7a538eef540cb97a7cc2
mitulty <mitultyagi45@gmail.com>
2025-06-07 15:13:58 +0530

    - Add code for bss and data init
    Signed-off-by: mitulty <mitultyagi45@gmail.com>

---
+++ firmware/system_init.c
@@ -0,0 +1,36 @@
+#include "stdint.h"
+
+extern char _data_flash_start;
+extern char _data_ram_start;
+extern char _data_ram_end;
+extern char _bss_start;
+extern char _bss_end;
+
+/**
+ * @brief Initializes the data and BSS sections of the system.
+ *
+ * This function copies the initialized data from flash to RAM and clears the BSS section.
+ * It is typically called during system startup to prepare the memory for use.
+ */
+void section_init(void)
+{
+    // Copy data section from flash to RAM
+    // This is typically done by the startup code, but can be done here if needed
+    uint32_t *src = &_data_flash_start;
+    uint32_t *dest = &_data_ram_start;
+    while (dest < &_data_ram_end)
+    {
+        *dest++ = *src++;
+    }
+
+    uint32_t *bss_start = &_bss_start;
+    uint32_t *bss_end = &_bss_end;
+
+    // Initialize BSS section to zero
+
+    while (bss_start < bss_end)
+    {
+        *bss_start++ = 0; // Set each word in BSS to 0
+    }
+    return;
+}
